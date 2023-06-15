f5aea2e6a36837633d73cfb3fe2b0e54d397b549
Maxim <toper.one111@gmail.com>
2023-06-15 13:27:30 +0300

    Start implementing cart item component

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-export const CartItem = () => {
-  return <div>CartItem</div>;
+export const CartItem = ({ id, img, title, price, amount }) => {
+  return (
+    <article className="cart-item">
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">${price}</h4>
+        <button className="remove-btn">remove</button>
+      </div>
+    </article>
+  );
 };
